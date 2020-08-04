6569f30b01d6f4c2695970f965556bd3c2f3fc58
chutithep88 <chutithep.job@gmail.com>
2020-08-04 15:04:27 +0700

    Updates

---
+++ precache-manifest.c4d8533081181734d693b65410a6e187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08a0e86af57d4d86fd5d45ac3306925",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "49c8d98307c99350a1a7",
+    "url": "/testReact/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "e10da691cafb9bdfd274",
+    "url": "/testReact/static/js/2.5175d211.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/testReact/static/js/2.5175d211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c8d98307c99350a1a7",
+    "url": "/testReact/static/js/main.a24dce93.chunk.js"
+  },
+  {
+    "revision": "c7548f0b01a45b7596aa",
+    "url": "/testReact/static/js/runtime-main.1d35be48.js"
+  }
+]);
